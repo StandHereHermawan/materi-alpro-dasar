4f91d84f191ed426d8d04695e80dbe4946744fe1
StandHereHermawan <nolifegansaya@gmail.com>
2023-11-08 14:32:21 +0700

    First Commit On This Repo, Initial Commit

---
+++ src/main/java/ariefbelajardikampus/materialprodasar/MateriAlproDasarApplication.java
@@ -0,0 +1,13 @@
+package ariefbelajardikampus.materialprodasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateriAlproDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateriAlproDasarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ariefbelajardikampus/materialprodasar/MateriAlproDasarApplicationTests.java
@@ -0,0 +1,13 @@
+package ariefbelajardikampus.materialprodasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateriAlproDasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
