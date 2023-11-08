357475462073cb782e4f2d397fc5bf9b0a04168d
StandHereHermawan <nolifegansaya@gmail.com>
2023-11-08 15:36:17 +0700

    Catatan, matrix tiga dimensi

---
+++ src/main/java/ariefbelajardikampus/materialprodasar/materi/array/multidimension/ThreeDimensionMatrix.java
@@ -0,0 +1,26 @@
+package ariefbelajardikampus.materialprodasar.materi.array.multidimension;
+
+public class ThreeDimensionMatrix {
+    public static void main(String[] args) {
+        int threeDMatrix[][][] = new int[3][4][5];
+        int i, j, k;
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                for (k = 0; k < 5; k++) {
+                    threeDMatrix[i][j][k] = i * j * k;
+                }
+            }
+        }
+
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 4; j++) {
+                for (k = 0; k < 5; k++) {
+                    System.out.print(threeDMatrix[i][j][k] + " " + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}
