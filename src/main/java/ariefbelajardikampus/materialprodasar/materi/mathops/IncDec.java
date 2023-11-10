065b9d9eb28d3f01d6488037ee9f7dec70439678
StandHereHermawan <nolifegansaya@gmail.com>
2023-11-10 19:44:24 +0700

    Catatan, pertemuan-5

--- src/main/java/ariefbelajardikampus/materialprodasar/materi/mathops/IncDec.java
+++ src/main/java/ariefbelajardikampus/materialprodasar/materi/mathops/IncDec.java
@@ -9,9 +9,9 @@ public class IncDec {
         c = ++b;
         d = a++;
         c++;
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
-        System.out.println("c = "+c);
-        System.out.println("d = "+d);
+        System.out.println("a = "+ a);
+        System.out.println("b = "+ b);
+        System.out.println("c = "+ c);
+        System.out.println("d = "+ d);
     }
 }
